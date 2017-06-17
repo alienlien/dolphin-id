eac14c5e68a622351492dff67d384a43e707fa98
Alien Lien <alienlien@gmail.com>
2017-06-17 20:39:50 +0800

    Add script to get data

---
+++ get_data.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+DATA_FOLDER=./data/
+wget -t 5 -O $DATA_FOLDER/20140911.zip https://www.dropbox.com/sh/f2l0dhlnok7ggkh/AABCfqQTNd7C1ixOCuBCjX2Ga/07_TT_20140811/CUT_TT20140811_pm_IFs?dl=1
+wget -t 5 -O $DATA_FOLDER/20110607.zip https://www.dropbox.com/sh/f2l0dhlnok7ggkh/AADYdNoizZoyf14gjl6G_6W_a/03_ST20110607_02_Gg%20%E4%BE%86\(PT\)/ST20110607_02_Gg_cut?dl=1
