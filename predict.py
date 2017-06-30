2baa475235a2c7091bd7b5505e9de6048e1e7ac1
Alien Lien <alienlien@gmail.com>
2017-06-30 20:01:41 +0800

    Introduce config into predict

--- predict.py
+++ predict.py
@@ -10,12 +10,13 @@ from keras.preprocessing import image
 from keras.models import load_model
 from keras.applications.inception_v3 import preprocess_input
 
+from config import ConfigStore
 
-target_size = (229, 229) #fixed size for InceptionV3 architecture
+target_size = (229, 229)  # Fixed size for InceptionV3 architecture
 
 
 def predict(model, img, target_size):
-  """Run model prediction on image
+    """Run model prediction on image
   Args:
     model: keras model
     img: PIL format image
@@ -23,55 +24,58 @@ def predict(model, img, target_size):
   Returns:
     list of predicted labels and their probabilities
   """
-  if img.size != target_size:
-    img = img.resize(target_size)
+    if img.size != target_size:
+        img = img.resize(target_size)
 
-  x = image.img_to_array(img)
-  x = np.expand_dims(x, axis=0)
-  x = preprocess_input(x)
-  preds = model.predict(x)
-  return preds[0]
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    x = preprocess_input(x)
+    preds = model.predict(x)
+    return preds[0]
 
 
-def plot_preds(image, preds):
-  """Displays image and the top-n predicted probabilities in a bar graph
+def plot_preds(image, preds, labels):
+    """Displays image and the top-n predicted probabilities in a bar graph
   Args:
     image: PIL image
     preds: list of predicted labels and their probabilities
   """
-  plt.imshow(image)
-  plt.axis('off')
+    plt.imshow(image)
+    plt.axis('off')
 
-  plt.figure()
-  labels = ("cat", "orca")
-  plt.barh([0, 1], preds, alpha=0.5)
-  plt.yticks([0, 1], labels)
-  plt.xlabel('Probability')
-  plt.xlim(0,1.01)
-  plt.tight_layout()
-  plt.show()
+    plt.figure()
+    plt.barh([0, 1], preds, alpha=0.5)
+    plt.yticks([0, 1], labels)
+    plt.xlabel('Probability')
+    plt.xlim(0, 1.01)
+    plt.tight_layout()
+    plt.show()
 
 
-if __name__=="__main__":
-  a = argparse.ArgumentParser()
-  a.add_argument("--image", help="path to image")
-  a.add_argument("--image_url", help="url to image")
-  a.add_argument("--model")
-  args = a.parse_args()
+if __name__ == "__main__":
+    a = argparse.ArgumentParser()
+    a.add_argument("--image", help="path to image")
+    a.add_argument("--image_url", help="url to image")
+    a.add_argument("--model")
+    a.add_argument('--config', help='Config key')
+    args = a.parse_args()
 
-  if args.image is None and args.image_url is None:
-    a.print_help()
-    sys.exit(1)
+    if args.image is None and args.image_url is None:
+        a.print_help()
+        sys.exit(1)
 
-  model = load_model(args.model)
-  if args.image is not None:
-    img = Image.open(args.image)
-    preds = predict(model, img, target_size)
-    plot_preds(img, preds)
+    cfg_store = ConfigStore()
+    config = cfg_store.get(args.config)
 
-  if args.image_url is not None:
-    response = requests.get(args.image_url)
-    img = Image.open(BytesIO(response.content))
-    preds = predict(model, img, target_size)
-    plot_preds(img, preds)
+    model = load_model(config['model'])
+    labels = config['labels']
+    if args.image is not None:
+        img = Image.open(args.image)
+        preds = predict(model, img, target_size)
+        plot_preds(img, preds, labels)
 
+    if args.image_url is not None:
+        response = requests.get(args.image_url)
+        img = Image.open(BytesIO(response.content))
+        preds = predict(model, img, target_size)
+        plot_preds(img, preds, labels)
