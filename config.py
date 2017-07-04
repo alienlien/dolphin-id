f2af25d97697c79d6f9f0869bec6efca3830a3f3
Alien Lien <alienlien@gmail.com>
2017-07-04 16:07:28 +0800

    Add flexibility for config

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 import os
 import os.path
+import sys
 import json
 from docopt import docopt
-
 
 DEFAULT_CONFIG_FILE = './config.json'
 
@@ -12,6 +12,7 @@ class ConfigStore:
     """Storage for configs.
        Note that we con't consider the multi-thread problem here.
     """
+
     def __init__(self, config_file=DEFAULT_CONFIG_FILE):
         if not os.path.exists(config_file):
             with open(config_file, 'w') as f:
@@ -28,43 +29,29 @@ class ConfigStore:
 
         return configs[key]
 
-    def get_data_dir(self, data_dir):
-        return self.get(get_key(data_dir))
-
     def set(self, config):
         with open(self.config_file, 'r') as f:
             configs = json.load(f)
 
         configs[config['name']] = config
         with open(self.config_file, 'w') as f:
-            json.dump(configs, f)
-
-    def set_data_dir(self, data_dir):
-        self.set(gen_config(data_dir))
+            json.dump(configs, f, indent=2)
 
 
 def get_labels(data_dir):
     return sorted(list(os.walk(data_dir))[0][1])
 
 
+def get_key(data_dir):
+    return os.path.basename(os.path.abspath(data_dir))
+
+
 def get_name(data_dir):
     return os.path.basename(os.path.abspath(data_dir))
 
 
-def get_key(data_dir):
-    return get_name(data_dir)
-
-
 def get_model(data_dir):
     return './model/inception-{k}.h5'.format(k=get_key(data_dir))
-
-
-def gen_config(data_dir):
-    return {
-        'name': get_name(data_dir),
-        'labels': get_labels(data_dir),
-        'model': get_model(data_dir),
-    }
 
 
 usage = """
@@ -72,12 +59,32 @@ Usage:
     config.py [options]
 
 Options:
-    --data_dir=DIR      The directory contains data. [default: ./data/20110607]
+    --data_dir=DIR  The directory contains data. [default: ./data/20110607]
+    --name=STR      The name of the config.
+    --model=FILE    The model file.
+
 """
 if __name__ == '__main__':
     args = docopt(usage, help=True)
-    data_dir = args['--data_dir']
+    data_dir, name, model = args['--data_dir'], args['--name'], args['--model']
+
+    if not data_dir:
+        print('One should provide data dir.')
+        sys.exit(0)
+
+    if not name:
+        name = get_name(data_dir)
+
+    if not model:
+        model = get_model(data_dir)
+
+    config = {
+        'name': name,
+        'data_dir': data_dir,
+        'model': model,
+        'labels': get_labels(data_dir),
+    }
 
     config_store = ConfigStore()
-    config_store.set_data_dir(data_dir)
-    print(config_store.get_data_dir(data_dir))
+    config_store.set(config)
+    print(config_store.get(name))
