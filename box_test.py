b546123665eb05c085abaa374d90f58dacbbcc93
Alien Lien <alienlien@gmail.com>
2017-08-11 14:06:34 +0800

    Remove via parser related functions from box.py

--- box_test.py
+++ box_test.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
-import json
-from box import Image, Box, parse_via_image
+from box import Box
 
 test_label = 'test'
 test_upper_left = (13.0, 17.0)
@@ -41,53 +40,3 @@ def test_box_init_3():
         upper_left=test_upper_left,
         lower_right=test_lower_right)
     assert_box(b)
-
-
-def test_parse_via_image():
-    root = 'aaa/bbb/ccc'
-    img_json_str = '''{
-        "fileref": "",
-        "size": 2785695,
-        "filename": "HL20100702_01_Gg_990702 (26).JPG",
-        "base64_img_data": "",
-        "file_attributes": {},
-        "regions": {
-            "0": {
-                "shape_attributes": {
-                    "name": "rect",
-                    "x": 1596,
-                    "y": 944,
-                    "width": 524,
-                    "height": 460
-                },
-                "region_attributes": {}
-            },
-            "1": {
-                "shape_attributes": {
-                    "name": "rect",
-                    "x": 3052,
-                    "y": 175,
-                    "width": 341,
-                    "height": 306
-                },
-                "region_attributes": {}
-            }
-        }
-    }'''
-    data = json.loads(img_json_str)
-    actual = parse_via_image(root, data)
-    expected = Image(
-        fname='aaa/bbb/ccc/HL20100702_01_Gg_990702 (26).JPG',
-        boxes=[
-            Box(
-                label=0,
-                upper_left=(1596, 944),
-                width=524,
-                height=460, ),
-            Box(
-                label=1,
-                upper_left=(3052, 175),
-                width=341,
-                height=306, ),
-        ])
-    assert actual == expected
