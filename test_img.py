1d74e03f333e34f460f3fe6f2da1f41431469e4d
Alien Lien <alienlien@gmail.com>
2017-08-16 20:00:57 +0800

    Add predict a folder

--- test_img.py
+++ test_img.py
@@ -4,17 +4,21 @@ import cv2
 from darkflow.net.build import TFNet
 
 IMAGE_FILE = os.path.abspath('./data/bounding-box/train/image/HL20100730_02_Gg_064_IMG_8928.JPG')
-print('>> Image file to process:', IMAGE_FILE)
+IMAGE_FOLDER = os.path.abspath('/Users/Alien/workspace/project/private/dolphin-id/data/bounding-box/src/HL20120708_01_gg_fix')
 
 options = {
     'model': 'config/tiny-yolo-dolphin.cfg',
     'load': -1,
     'threshold': 0.1,
     'labels': './labels_dolphin.txt',
+    'imgdir': IMAGE_FOLDER,
+    'json': True,
 }
 
 tfnet = TFNet(options)
 
-imgcv = cv2.imread(IMAGE_FILE)
-result = tfnet.return_predict(imgcv)
-print(result)
+tfnet.predict()
+
+# imgcv = cv2.imread(IMAGE_FILE)
+# result = tfnet.return_predict(imgcv)
+# print(result)
