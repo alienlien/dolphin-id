2467699003cb2bc7189c622d46867ab9961365d1
Alien Lien <alienlien@gmail.com>
2017-08-18 18:29:03 +0800

    Correct the data path for fin detector training script

--- train_detector.sh
+++ train_detector.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+# It is used to train the fin detector for the dolphins
 flow --model ./config/tiny-yolo-dolphin.cfg \
     --config ./config/ \
     --load ./bin/tiny-yolo-voc.weights \
     --labels labels_dolphin.txt \
     --train \
-    --annotation ./data/bounding-box/train/annotation \
-    --dataset ./data/bounding-box/train/image
+    --annotation ./data/detector/train/annotation \
+    --dataset ./data/detector/train/image
