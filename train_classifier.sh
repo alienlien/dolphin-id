36c3336750c3ed5a0b8e3e778c8c82eb32b31de1
Alien Lien <alienlien@gmail.com>
2017-09-13 17:39:34 +0800

    Fix the number of epoch for classifier training and add comment

--- train_classifier.sh
+++ train_classifier.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+# Note that the classifier would fine tune the upper half of the model
+# after lower half of the model finished training of the [nb_epoch] epochs.
+# As a result, nb_epoch cannot be set too large.
 python ./train_classifier.py \
     --train_dir=./data/classifier/train/ \
     --val_dir=./data/classifier/validation/ \
-    --nb_epoch=500 \
+    --nb_epoch=50 \
     --batch_size=16 \
     --output_model_file=./model/dolphin_classifier.h5
