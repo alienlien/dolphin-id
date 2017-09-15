7c0da32307627bbbca0db78bd81accb35176ae34
Alien Lien <alienlien@gmail.com>
2017-09-15 20:55:27 +0800

    Add todo for detector training script

--- train_detector.sh
+++ train_detector.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # It is used to train the fin detector for the dolphins
+# TODO: Add the selection for the latest model or the original one.
+# Ref: https://github.com/thtrieu/darkflow#training-new-model
 flow --model ./config/yolo-dolphin.cfg \
     --config ./config/ \
     --load ./bin/yolo-voc.weights \
