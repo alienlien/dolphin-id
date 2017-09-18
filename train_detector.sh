03017f91ec003695257557f6bcb860ec083300cc
Alien Lien <alienlien@gmail.com>
2017-09-18 17:34:28 +0800

    Add note to load latest model for detector training shell script

--- train_detector.sh
+++ train_detector.sh
@@ -2,6 +2,7 @@
 # It is used to train the fin detector for the dolphins
 # TODO: Add the selection for the latest model or the original one.
 # Ref: https://github.com/thtrieu/darkflow#training-new-model
+# NOTE: Remember to modify use option '--load -1' to continue or not.
 flow --model ./config/yolo-dolphin.cfg \
     --config ./config/ \
     --load ./bin/yolo-voc.weights \
