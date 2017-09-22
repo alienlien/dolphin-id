bb8fc97eb04a8b9a20ca30308c98b59b572aa786
Alien Lien <alienlien@gmail.com>
2017-09-22 22:59:04 +0800

    Add a box drawler with bounding box and label

--- draw.py
+++ draw.py
@@ -1,12 +1,45 @@
 #!/usr/bin/env python3
 # It is used to draw the boxes on the pic.
-# import os.path
-# import sys
 from docopt import docopt
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw, ImageFont
 from parser import from_xml
 
 DEFAULT_WIDTH = 20  # Width for the boxes.
+DEFAULT_FONT_PATH = '/Library/Fonts/Arial.ttf'
+DEFAULT_FONT_SIZE = 100
+
+
+class BoxDrawer():
+    def __init__(self,
+                 font_path=DEFAULT_FONT_PATH,
+                 font_size=DEFAULT_FONT_SIZE):
+        self.font = ImageFont.truetype(font=font_path, size=font_size)
+
+    def draw(self, img_in, img_out, boxes, width, color):
+        """It draws boxes on the image input.
+
+        Args:
+            img_in: The path of the image input.
+        """
+        img = Image.open(img_in)
+        draw = ImageDraw.Draw(img)
+
+        for box in boxes:
+            # Render the bounding box.
+            (ulx, uly), (lrx, lry) = box.upper_left(), box.lower_right()
+            for shift in range(0, width):
+                draw.rectangle(
+                    [(ulx + shift, uly + shift), (lrx + shift, lry + shift)],
+                    outline=color)
+
+            # Render the label.
+            label = box.label()
+            _, height = self.font.getsize(label)
+            draw.text(
+                (lrx, uly - height), box.label(), fill=color, font=self.font)
+
+        img.save(img_out, 'PNG')
+
 
 usage = """
 Usage:
@@ -15,26 +48,13 @@ Usage:
 Options:
     --img=FILE  The file to draw.
     --anno=FILE The annotation file (.xml).
-    --out=FILE  Output file.
+    --out=FILE  Output file [default: ./test.png].
 """
 if __name__ == '__main__':
     args = docopt(usage, help=True)
+    drawer = BoxDrawer()
 
     with open(args['--anno'], 'r') as f:
         boxes = from_xml(f).boxes
 
-    img = Image.open(args['--img'])
-    draw = ImageDraw.Draw(img)
-
-    for box in boxes:
-        (ulx, uly), (lrx, lry) = box.upper_left(), box.lower_right()
-        width = DEFAULT_WIDTH
-        for shift in range(0, width):
-            draw.rectangle(
-                [(ulx + shift, uly + shift), (lrx + shift, lry + shift)],
-                outline='yellow')
-
-    out_file = './output.png'
-    if args['--out']:
-        out_file = args['--out']
-    img.save(out_file, 'PNG')
+    drawer.draw(args['--img'], args['--out'], boxes, DEFAULT_WIDTH, 'yellow')
