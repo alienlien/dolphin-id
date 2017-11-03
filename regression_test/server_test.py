72c4b7cdf89b181f18d0131e06948e40e8a3587a
Alien Lien <alienlien@gmail.com>
2017-11-04 01:31:35 +0800

    Fix issues about lib in reg test

--- regression_test/server_test.py
+++ regression_test/server_test.py
@@ -5,18 +5,17 @@ import json
 import requests
 from urllib.parse import urljoin
 from google.protobuf import json_format
-import sys
-print('sys.path:', sys.path)
+# Note that the importing only works when
+# 1) pytest in the source dir (repo root)
+# 2) ./test.sh: Set the environ var PYTHONPATH as repo root.
+# Ref: https://stackoverflow.com/questions/25827160/importing-correctly-with-pytest
 import proto.image_pb2 as pb
-# from .. import proto.image_pb2 as pb
-# import ..proto.image_pb2 as pb
 
 API_URL = 'http://localhost:5000'
 PRED_IMAGE_API = 'prediction/image/'
 PRED_FIN_API = '/prediction/image/fin/'
-# TEST_IMAGE = '/Users/Alien/workspace/project/private/dolphin-id/data/detector/validation/image/HL20100702_01_Gg_990702 (103)'
-# TEST_IMAGE_FIN = '/Users/Alien/workspace/project/private/dolphin-id/data/classifier/src/20080713/01_Ku N000/st20080713_06_Gg_HY_ 228_a.JPG'
-TEST_IMAGE = './test.jpg'
+TEST_IMAGE = '../data/detector/src/HL20100702_01/HL20100702_01_Gg_990702 (103).JPG'
+TEST_IMAGE_FIN = '../data/classifier/merge/ku_304/HL20110820_02_gg_021_IMG_4801_0.jpg'
 
 
 def assert_region(item):
@@ -24,6 +23,37 @@ def assert_region(item):
     json_format.Parse(json.dumps(item), region)
     assert region.shape == pb.Region.RECTANGLE
 
+
+# It tests the following image prediction API.
+# Request:
+# curl -X POST \
+#   -H "X-Application-Id: ${APPLICATION_ID}" \
+#   -H "X-REST-API-Key: ${REST_API_KEY}" \
+#   -F "file=@${file path}" \
+#   http://${server url:port}/prediction/image/
+#
+# Response:
+# [					// List of regions in the image
+#   {
+#     "shape": enum (RECTANGLE),	// Region shape. Json format: string.
+#     "rectangleProperty": {		// Property of a rectangle.
+#       "upperLeftX": float,		// X-axis of the upper left corner.
+#       "upperLeftY": float,		// Y-axis of the upper left corner.
+#       "width": float,			// Width of the rectangle.
+#       "height": float			// Height of the rectangle.
+#     },
+#     "dolphinId": string,		// Dolphin ID.
+#     "groupId": int,			// ID w.r.t. that sighting.
+#     "predictions": [			// List of dolphin ID predicted.
+#       {
+#         "dolphinId": string,	        // Dolphin ID.
+#         "prob": float			// Probability of the prediction.
+#       },
+#       ...
+#     ]
+#   },
+#   ...
+# ]
 def test_pred_image():
     url = urljoin(API_URL, PRED_IMAGE_API)
     files = {
@@ -37,12 +67,29 @@ def test_pred_image():
     for item in results:
         assert_region(item)
 
+
+# It tests the following fin prediction API.
+# Request:
+# curl -X POST \
+#   -H "X-Application-Id: ${APPLICATION_ID}" \
+#   -H "X-REST-API-Key: ${REST_API_KEY}" \
+#   -F "file=@${file path}" \
+#   http://${server url:port}/prediction/image/fin/
+#
+# Response:
+# [				// List of dolphin ID predicted.
+#   {
+#     "dolphinId": string,	// Dolphin ID.
+#     "prob": float		// Probability of the prediction.
+#   },
+#   ...
+# ]
 def test_pred_fin():
     """It tests the fin prediction API.
     """
     url = urljoin(API_URL, PRED_FIN_API)
     files = {
-        'file': open(TEST_IMAGE, 'rb'),
+        'file': open(TEST_IMAGE_FIN, 'rb'),
     }
     resp = requests.post(url, files=files)
     assert resp.status_code == HTTPStatus.OK
